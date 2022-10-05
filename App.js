2b321e4c6edb2ff006b99aa5c2bed76cd74b5de5
pinkjagnie <olga.krzysztofik@gmail.com>
2022-10-05 18:00:55 +0200

    Fetching users from API

--- App.js
+++ App.js
@@ -1,13 +1,35 @@
-import React from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { StyleSheet, View, Alert } from 'react-native';
 import Constants from 'expo-constants';
+
+import axios from 'axios';
 
 import TopBar from './components/TopBar';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const { data } = await axios.get('https://randomuser.me/api/?gender=female&results=50')
+      setUsers(data.results);
+      // console.log(data.results)
+    } catch (error) {
+      console.log(error)
+      Alert.alert('Error getting users', '', [{ text: 'Retry', onPress: () => fetchUsers() }])
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers()
+  }, []);
+
   return (
     <View style={styles.container}>
       <TopBar />
+      <View style={styles.swipes}>
+
+      </View>
     </View>
   );
 }
@@ -17,4 +39,17 @@ const styles = StyleSheet.create({
     flex: 1,
     marginTop: Constants.statusBarHeight,
   },
+  swipes: {
+    flex: 1,
+    padding: 10,
+    paddingTop: 8,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 3,
+    },
+    shadowOpacity: 0.29,
+    shadowRadius: 4.65,
+    elevation: 7,
+  }
 });
